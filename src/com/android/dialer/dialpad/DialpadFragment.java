f9041581c9a0a34fb2485c6a68e8a3f33acb7f54
0xD34D <clark@scheffsblend.com>
2013-07-31 12:29:06 -0700

    Update dialer layout for theme support
    Change-Id: I0760bfa8ad0bfc6138af6b7626526d7ff2078ffc

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -16,6 +16,9 @@
 
 package com.android.dialer.dialpad;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -54,6 +57,9 @@ import android.text.TextWatcher;
 import android.text.style.RelativeSizeSpan;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -162,6 +168,8 @@ public class DialpadFragment extends Fragment
      * smart dialing suggestion strip is visible.
      */
     private boolean mSmartDialEnabled = false;
+
+    private boolean mSmartDialHidden = false;
 
     /**
      * Regular expression prohibiting manual phone call. Can be empty, which means "no rule".
@@ -994,7 +1002,7 @@ public class DialpadFragment extends Fragment
                 return false;
             }
             case R.id.dialButton: {
-                if (isDigitsEmpty()) {
+                if (!mSmartDialHidden && isDigitsEmpty()) {
                     handleDialButtonClickWithEmptyDigits();
                     // This event should be consumed so that onClick() won't do the exactly same
                     // thing.
